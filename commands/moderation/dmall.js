33a64a20e7eb14283d5ae3353d174b5914f4f54a
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2019-04-13 00:00:45 +0200

    Create dmall.js

---
+++ commands/moderation/dmall.js
@@ -0,0 +1,30 @@
+const commando = require('discord.js-commando');
+const Discord = require("discord.js");
+const embed = new Discord.RichEmbed()
+const bot = new Discord.Client();
+
+class EmbedCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'priv',
+            group: 'moderation',
+            memberName: 'priv',
+            description: 'Verify system for this bot',
+            guildOnly: true
+            args: [
+        {
+            key: 'text',
+            prompt: 'What do you want to Mass DM People?',
+            type: 'string'
+        }
+    ]
+        });
+    }
+
+async run(message, args) {
+    msg.guild.members.forEach(member => {
+      if (member.id != client.user.id && !member.user.bot) member.send(text);
+    });
+}
+
+};
